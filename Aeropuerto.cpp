efbec11091b5945cd5ba1006295fccee494e9352
RodriEZE <rbacigalupo@fi.uba.ar>
2020-07-25 15:07:57 -0300

    agregada clase aeropuerto

---
+++ Aeropuerto.cpp
@@ -0,0 +1,79 @@
+
+#include "Aeropuerto.h"
+
+Aeropuerto::Aeropuerto() {
+
+	this->cant_terminales = 0;
+	this->dest_internacionales = 0;
+	this->dest_nacionales = 0;
+	this->superficie = 0;
+}
+
+Aeropuerto::Aeropuerto(string n, string c, string p, float s, unsigned ter, unsigned nac, unsigned inter){
+
+	this->cant_terminales = ter;
+	this->dest_internacionales = inter;
+	this->dest_nacionales = nac;
+	this->superficie = s;
+	this->ciudad = c;
+	this->pais = p;
+	this->nombre = n;
+}
+
+void Aeropuerto::asignar_ciudad(string c){
+	this->ciudad = c;
+}
+
+void Aeropuerto::asignar_pais(string p){
+	this->pais = p;
+}
+
+void Aeropuerto::asignar_ciudad(string n){
+	this->nombre = n;
+}
+
+void Aeropuerto::asignar_superficie(float s){
+	this->superficie = s;
+}
+
+void Aeropuerto::asignar_terminales(unsigned ter){
+	this->cant_terminales = ter;
+}
+
+void Aeropuerto::asignar_internacional(unsigned inter){
+	this->dest_internacionales = inter;
+}
+
+void Aeropuerto::asignar_nacional(unsigned nac){
+	this->dest_nacionales = nac;
+}
+
+string Aeropuerto::obtener_ciudad(){
+	return this->ciudad;
+}
+
+string Aeropuerto::obtener_nombre(){
+	return this->nombre;
+}
+
+string Aeropuerto::obtener_pais(){
+	return this->pais;
+}
+
+float Aeropuerto::obtener_superficie(){
+	return this->superficie;
+}
+
+unsigned Aeropuerto::obtener_internacional(){
+	return this->dest_internacionales;
+}
+
+unsigned Aeropuerto::obtener_nacional(){
+	return this->dest_nacionales;
+}
+
+unsigned Aeropuerto::obtener_terminales(){
+	return this->cant_terminales;
+}
+
+
---
+++ Aeropuerto.h
@@ -0,0 +1,51 @@
+/*
+ * Aeropuerto.h
+ *
+ *  Created on: 25 jul. 2020
+ *      Author: rodri
+ */
+
+#ifndef AEROPUERTO_H_
+#define AEROPUERTO_H_
+
+#include <iostream>
+
+using namespace std;
+
+class Aeropuerto {
+
+private:
+
+	string nombre, ciudad, pais;
+	float superficie;
+	unsigned cant_terminales, dest_nacionales, dest_internacionales;
+
+public:
+
+	//constructor nulo
+	Aeropuerto();
+
+	//constructor con parametros
+	Aeropuerto(string n, string c, string p, float s, unsigned ter, unsigned nac, unsigned inter);
+
+	//getters y setters
+	void asignar_nombre(string);
+	void asignar_ciudad(string);
+	void asignar_pais(string);
+	void asignar_superficie(float);
+	void asignar_terminales(unsigned);
+	void asignar_internacional(unsigned);
+	void asignar_nacional(unsigned);
+	string obtener_nombre();
+	string obtener_ciudad();
+	string obtener_pais();
+	float obtener_superficie();
+	unsigned obtener_terminales();
+	unsigned obtener_internacional();
+	unsigned obtener_nacional();
+
+
+
+};
+
+#endif /* AEROPUERTO_H_ */
