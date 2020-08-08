4463baf2814e2663944b89eee59f143ecd7352ea
RodriEZE <rbacigalupo@fi.uba.ar>
2020-08-08 20:55:33 -0300

    agregada opcion de prioridad en cola

--- Cola.h
+++ Cola.h
@@ -36,10 +36,15 @@ public:
 	// POST : agrega un dato ( dentro de un nodo ) al final
 	void insertar(T d);
 
+	// Agrega un elemento a la cola con prioridad
+	// POST : agrega un dato al final de la cola con la prioridad designada
+	void insertar(T d, int prioridad);
+
 	// Obtener el dato que está al principio
 	// PRE : - cola creada y no vacía
 	// POST : devuelve el dato que está al principio
 	T obtenerDato ();
+
 	// Borrado del nodo que está al principio
 	// PRE : - cola creada y no vacía
 	// POST : libera el nodo que está al principio
@@ -74,6 +79,29 @@ void Cola<T>:: insertar(T d) {
 		ultimo -> establecer_siguiente ( pnodo );
 	ultimo = pnodo ;
 	tam ++;
+}
+
+template<class T>
+void Cola<T>:: insertar(T d, int prioridad) {
+
+	Nodo<T>* pnodo = new Nodo<T>(d,prioridad);
+
+	if (this -> esVacia ()) {
+
+		primero = pnodo ;
+
+	}else{
+
+		Nodo<T>* aux = primero;
+
+		while((aux->obtener_siguiente() != NULL) && (aux->obtener_siguiente()->obtener_prioridad()>prioridad)){
+
+			aux = aux->obtener_siguiente();
+		}
+
+		pnodo->establecer_siguiente(aux->obtener_siguiente());
+		aux->establecer_siguiente(pnodo);
+	}
 }
 
 template<class T>
--- Nodo.h
+++ Nodo.h
@@ -11,6 +11,7 @@ class Nodo {
 
 	private:
 		T dato;
+		int prioridad;
 		Nodo<T>* psig;
 
 	public:
@@ -19,6 +20,9 @@ class Nodo {
 		//Crea un nodo con el dato que recibe e inicializa siguiente en NULL
 		Nodo(T dato_nuevo);
 
+		//Post: Crea un nodo con dato_nuevo y prioridad p
+		Nodo(T dato_nuevo, int p);
+
 		//Pre: Existe el nodo
 		//Post: Devuelve un dato T
 		T obtener_dato();
@@ -26,6 +30,9 @@ class Nodo {
 		//Pre: Existe el nodo
 		//Post: Devuelve el puntero al siguiente nodo, si es el ultimo devuelve NULL
 		Nodo<T>* obtener_siguiente();
+
+		//Post: devuelve la prioridad del nodo.
+		int obtener_prioridad();
 
 		//Pre: -
 		//Post: El puntero al siguiente nodo apuntara a psig
@@ -54,9 +61,23 @@ Nodo<T>* Nodo<T>::obtener_siguiente(){
 }
 
 template<class T>
+int Nodo<T>::obtener_prioridad(){
+	return this->prioridad;
+}
+
+template<class T>
 Nodo<T>::Nodo(T dato_nuevo){
 	dato = dato_nuevo;
 	this->psig = NULL;
+	prioridad = 1;
+}
+
+template<class T>
+Nodo<T>::Nodo(T dato_nuevo, int d){
+
+	dato = dato_nuevo;
+	this->psig = NULL;
+	prioridad = d;
 }
 
 template<class T>
